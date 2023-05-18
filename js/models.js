5dcaaa3d2319308c409d7886d02ef01973970ef1
1aid <aidenbrown@usf.edu>
2023-05-18 12:25:10 -0400

    firstcommit

---
+++ js/models.js
@@ -0,0 +1,165 @@
+"use strict";
+
+const BASE_URL = "https://hack-or-snooze-v3.herokuapp.com";
+
+/******************************************************************************
+ * Story: a single story in the system
+ */
+
+class Story {
+  constructor({ storyId, title, author, url, username, createdAt }) {
+    this.storyId = storyId;
+    this.title = title;
+    this.author = author;
+    this.url = url;
+    this.username = username;
+    this.createdAt = createdAt;
+  }
+
+  getHostName() {
+    return new URL(this.url).host;
+  }
+}
+
+
+/******************************************************************************
+ * List of Story instances: used by UI to show story lists in DOM.
+ */
+
+class StoryList {
+  constructor(stories) {
+    this.stories = stories;
+  }
+
+  static async getStories() {
+    const response = await axios({
+      url: `${BASE_URL}/stories`,
+      method: "GET",
+    });
+
+    const stories = response.data.stories.map(story => new Story(story));
+
+    return new StoryList(stories);
+  }
+
+  async addStory(user, { title, author, url }) {
+    const token = user.loginToken;
+    const response = await axios({
+      method: "POST",
+      url: `${BASE_URL}/stories`,
+      data: { token, story: { title, author, url } },
+    });
+
+    const story = new Story(response.data.story);
+    this.stories.unshift(story);
+    user.ownStories.unshift(story);
+
+    return story;
+  }
+
+  async removeStory(user, storyId) {
+    const token = user.loginToken;
+    await axios({
+      url: `${BASE_URL}/stories/${storyId}`,
+      method: "DELETE",
+      data: { token: user.loginToken }
+    });
+
+    this.stories = this.stories.filter(story => story.storyId !== storyId);
+    user.ownStories = user.ownStories.filter(s => s.storyId !== storyId);
+    user.favorites = user.favorites.filter(s => s.storyId !== storyId);
+  }
+}
+
+
+/******************************************************************************
+ * User: a user in the system (only used to represent the current user)
+ */
+
+class User {
+  constructor(
+    {
+      username,
+      name,
+      createdAt,
+      favorites = [],
+      ownStories = []
+    },
+    token
+  ) {
+    this.username = username;
+    this.name = name;
+    this.createdAt = createdAt;
+    this.favorites = favorites.map(s => new Story(s));
+    this.ownStories = ownStories.map(s => new Story(s));
+    this.loginToken = token;
+  }
+
+  static async signup(username, password, name) {
+    const response = await axios({
+      url: `${BASE_URL}/signup`,
+      method: "POST",
+      data: { user: { username, password, name } },
+    });
+
+    let { user } = response.data;
+
+    return new User(
+      {
+        username: user.username,
+        name: user.name,
+        createdAt: user.createdAt,
+        favorites: user.favorites,
+        ownStories: user.stories
+      },
+      response.data.token
+    );
+  }
+
+  static async login(username, password) {
+    const response = await axios({
+      url: `${BASE_URL}/login`,
+      method: "POST",
+      data: { user: { username, password } },
+    });
+
+    let { user } = response.data;
+
+    return new User(
+      {
+        username: user.username,
+        name: user.name,
+        createdAt: user.createdAt,
+        favorites: user.favorites,
+        ownStories: user.stories
+      },
+      response.data.token
+    );
+  }
+
+  static async loginViaStoredCredentials(token, username) {
+    try {
+      const response = await axios({
+        url: `${BASE_URL}/users/${username}`,
+        method: "GET",
+        params: { token },
+      });
+      
+      let { user } = response.data;
+
+      return new User(
+        {
+          username: user.username,
+          name: user.name,
+          createdAt: user.createdAt,
+          favorites: user.favorites,
+          ownStories: user.stories
+        },
+        token
+      );
+    } catch (err) {
+      console.error("loginViaStoredCredentials failed", err);
+      return null;
+    }
+  }
+}
